6e95a628371fec2c56fe7df39ab7eb307cdfc7dc
upinetree <t.matsumoto@everyleaf.com>
2014-05-24 17:29:10 +0900

    bundle gem

---
+++ lib/fluentd/plugin/nicorepo.rb
@@ -0,0 +1,9 @@
+require "fluentd/plugin/nicorepo/version"
+
+module Fluentd
+  module Plugin
+    module Nicorepo
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluentd/plugin/nicorepo/version.rb
@@ -0,0 +1,7 @@
+module Fluentd
+  module Plugin
+    module Nicorepo
+      VERSION = "0.0.1"
+    end
+  end
+end
