e03279af2be94c47b4d716a9c1780436704c1e49
upinetree <t.matsumoto@everyleaf.com>
2014-06-04 23:05:11 +0900

    Implement simple thread emission

--- lib/fluentd/plugin/in_nicorepo.rb
+++ lib/fluentd/plugin/in_nicorepo.rb
@@ -3,6 +3,37 @@ require 'nicorepo'
 module Fluentd
   class NicorepoInput < Fluent::Input
     Plugin.register_input 'nicorepo' self
+
+    config_param :tag, :string
+
+    def configure(conf)
+      @interval = 3
+      super
+    end
+
+    def start
+      @thread = Thread.new(&method(:run))
+      super
+    end
+
+    def shutdown
+      super
+    end
+
+    def run
+      loop do
+        Thread.new(&method(:run))
+        sleep @interval
+      end
+    end
+
+    def run
+      time = Time.now.to_s
+      record = "hoge"
+      Engine.emit(tag, time, record)
+    rescue => e
+      log.error "unexpected error", error: e
+    end
   end
 end
 
